52819933c4b2fca5fa8ae9b78f0fa7096d499a01
hassan-raza <hassan.numl2013@gmail.com>
2021-01-06 03:26:04 +0500

    url updated

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,8 @@
 MIT License
 Copyright (c) 2019 - present AppSeed.us
 """
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include  # add this
 
@@ -11,4 +12,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("authentication.urls")),  # add this
     path("", include("app.urls"))  # add this
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
